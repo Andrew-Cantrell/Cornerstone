28cc459198ea7e242dbf90c78d689d71c188f49e
Andrew <32985893+Andrew-Cantrell@users.noreply.github.com>
2017-11-22 12:27:31 -0500

    Add files via upload

---
+++ Cornerstone v0.1.py
@@ -0,0 +1,11 @@
+import sys
+import random
+import os
+
+age = 22
+
+if age >= 21:
+    print('You are old enough to drive a car and a commercial vehicle')
+elif age >= 16:
+    print ('You are old enough to drive a car')
+else : print('You are not old enough to drive')
