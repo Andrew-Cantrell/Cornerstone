9a68d74475b538d2d8a76c3ff7197f161bdc1954
Andrew <32985893+Andrew-Cantrell@users.noreply.github.com>
2017-11-25 20:55:05 -0500

    blam
    11/26

--- survey2.py
+++ survey2.py
@@ -5,7 +5,7 @@
 age = int(input("Enter your age: "))
 heightFEET = int(input("What is your height - Feet: "))
 heightINCHES = int(input("What is your height - Inches: "))
-weight = int(input("Choose your weight scale - LBs or KGs "))
+weight = int(input("Choose your weight scale - LBs or KGs# "))
 
 militarystatus = type(input("Did you serve in the military? Y/N: "))
 gender = type(input("Please enter your gender. M/F/O: "))
